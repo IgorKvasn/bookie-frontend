23806727782f695589200fe989265c1c827812a2
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2016-12-27 16:43:44 +0100

    fixed tests

--- tests/unit/helpers/old-hour-test.js
+++ tests/unit/helpers/old-hour-test.js
@@ -1,12 +1,51 @@
-
-import { oldHour } from 'tennis-manager/helpers/old-hour';
-import { module, test } from 'qunit';
+import {
+    oldHour
+} from 'tennis-manager/helpers/old-hour';
+import {
+    module,
+    test
+} from 'qunit';
 
 module('Unit | Helper | old hour');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = oldHour([42]);
-  assert.ok(result);
+[{
+    testName: 'works for old day',
+    hour: 10,
+    currentHour: 20,
+    day: 1,
+    currentDay: 4,
+    expectedResult: true
+}, {
+    testName: 'works for new day',
+    hour: 10,
+    currentHour: 20,
+    day: 5,
+    currentDay: 4,
+    expectedResult: false
+}, {
+    testName: 'works for same day, old hour',
+    hour: 10,
+    currentHour: 20,
+    day: 5,
+    currentDay: 5,
+    expectedResult: true
+}, {
+    testName: 'works for same day, new hour',
+    hour: 21,
+    currentHour: 20,
+    day: 5,
+    currentDay: 5,
+    expectedResult: false
+}, {
+    testName: 'works for same day, same hour',
+    hour: 21,
+    currentHour: 21,
+    day: 5,
+    currentDay: 5,
+    expectedResult: false
+}].forEach((testConfig) => {
+    test(testConfig.testName, function(assert) {
+        let result = oldHour(null, testConfig);
+        assert.equal(result, testConfig.expectedResult);
+    });
 });
-
