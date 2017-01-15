e0b55974002a398de2f7299738c2f07a10adefa2
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2017-01-15 16:20:44 +0100

    ember-data removed - tests fixed

--- app/serializers/court.js
+++
@@ -1,9 +0,0 @@
-import SpringSerializer from './abstract-spring-serializer';
-
-export default SpringSerializer.extend({
-  attrs: {
-    'reservations': {
-      embedded: 'always'
-    }
-  }
-});
